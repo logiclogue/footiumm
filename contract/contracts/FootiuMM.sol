517e5d54e74f5548fcc946015f7dba7ed9f78bc8
James <jamesoleary32@gmail.com>
2024-03-09 13:03:29 +0000

    correctly instantiate NFT contract on the AMM

--- contract/contracts/FootiuMM.sol
+++ contract/contracts/FootiuMM.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
@@ -13,6 +14,7 @@ import "@openzeppelin/contracts/utils/Address.sol";
 contract FootiuMM is IERC721Receiver {
 
     using Address for address payable;
+    ERC721 public nftContract;
 
     address public dependencyAddress;
 
@@ -26,12 +28,12 @@ contract FootiuMM is IERC721Receiver {
 
     event Create(address indexed nft);
     event NftDeposited(address indexed user, address indexed nftContract, uint256 indexed tokenId);
+    event NFTPurchased(address indexed user, uint256 indexed tokenId);
 
 
     constructor(address _dependencyAddress) payable {
         require(msg.value >= 0.1 ether, "Insufficient initial ETH sent");
-
-        dependencyAddress = _dependencyAddress;
+        nftContract = ERC721(_dependencyAddress);
     }
 
     /* Implementing IERC721Receiver*/
@@ -49,11 +51,20 @@ contract FootiuMM is IERC721Receiver {
         // Store the deposited NFT
         nftDeposits[msg.sender].push(NftDeposit(tokenId));
 
-
-
         // Emit event
         emit NftDeposited(msg.sender, dependencyAddress, tokenId);
     }
+
+
+    function buyNFT(uint256 _tokenId) external {
+
+        // Transfer ownership of the NFT to the buyer
+        nftContract.transferFrom(address(this), msg.sender, _tokenId);
+
+        emit NFTPurchased(msg.sender, _tokenId);
+    }
+
+
 
     /* Implementing Getter Functions  */
     function getContractBalance() external view returns (uint256) {
