a860ba4df632d2814bbf5d9967825ee71ae0a9f0
logiclogue <jordanpaullord@hotmail.co.uk>
2024-03-09 18:32:28 +0000

    Adds a deployment script

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,6 +1,38 @@
+require("hardhat-deploy");
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.24",
+const config = {
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.24",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200
+                    }
+                }
+            }
+        ]
+    },
+    namedAccounts: {
+        deployer: 0
+    },
+    networks: {
+        localhost: {
+            url: "http://0.0.0.0:8545"
+        },
+        arbitrum: {
+            url: "https://arb1.arbitrum.io/rpc",
+            chainId: 42161,
+            accounts: process.env.TEST_ETH_KEY ? [process.env.TEST_ETH_KEY] : []
+        },
+        "arbitrum-sepolia": {
+            url: "https://sepolia-rollup.arbitrum.io/rpc",
+            chainId: 421614,
+            accounts: process.env.TEST_ETH_KEY ? [process.env.TEST_ETH_KEY] : []
+        }
+    }
 };
+
+module.exports = config;
--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -7,22 +7,14 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+    const FootiuMM = await hre.ethers.deployContract("FootiuMM", [unlockTime], {
+        _dependencyAddress: FootiumPlayerAddress,
+        _decayRate: 2
+    });
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+    await FootiuMM.waitForDeployment();
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+    console.log("FootiuMM deployed", FootiuMM);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
