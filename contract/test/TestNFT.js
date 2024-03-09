e3ea928e5e58df68b4b260111c70ec6a8e14c911
James <jamesoleary32@gmail.com>
2024-03-09 11:30:00 +0000

    test NFT contract + unit tests

--- contract/test/TestNFT.js
+++ contract/test/TestNFT.js
@@ -1,9 +1,22 @@
 const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
 describe("TestNFT", function() {
 
     it("should return correct name", async function () {
-        
+      const MyContract = await ethers.getContractFactory("TestNFT");
+      
+      const myContractDeployed = await MyContract.deploy("MyContractName", "MCN");
+
+      expect(await myContractDeployed.name()).to.equal("MyContractName")
     })
+    
+    it("should return correct symbol", async function () {
+        const MyContract = await ethers.getContractFactory("TestNFT");
+        
+        const myContractDeployed = await MyContract.deploy("MyContractName", "MCN");
+  
+        expect(await myContractDeployed.symbol()).to.equal("MCN")
+      })
 
 })
