c82b1f43c56c2f01b79b4e15a34da4a8b364c45c
logiclogue <jordanpaullord@hotmail.co.uk>
2024-03-09 20:38:25 +0000

    Fixes up the deploy script and deploys FootiuMM

---
+++ contract/deploy/deploy.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy } = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    const result = await deploy('FootiuMM', {
+        from: deployer,
+        args: ["0xD65f8B2f2Be08564764D6585B4700554b6596Da3", 2],
+        log: true
+    });
+
+    console.log("HERE", result);
+};
+
+module.exports.tags = ['FootiuMM'];
